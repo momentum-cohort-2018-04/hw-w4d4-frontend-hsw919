70119bd355ad0a28d524d90832b5ec46accde722
hsw919 <hunterwoody@gmail.com>
2018-05-18 17:00:54 -0400

    Have the search and the audio working correctly

---
+++ script.js
@@ -0,0 +1,42 @@
+import $ from 'jquery'
+import request from 'superagent'
+
+$(document).ready(function () {
+  function getAndSetValue () {
+    const searchArray = $('#search').val().split(' ')
+    const search = searchArray.join('+')
+
+    request
+      .get(`https://itunes.apple.com/search?term=${search}&media=music`)
+      .then(response => {
+        let parsedResponse = JSON.parse(response.text)
+        let parsedArray = parsedResponse.results
+        $('.music').html('')
+        for (var i = 0; i < parsedArray.length; i++) {
+          // console.log(parsedArray[i])
+          $('.music').append(`
+            <div class="track" data-preview="${parsedArray[i].previewUrl}">
+              <img class="track__img" src="${parsedArray[i].artworkUrl100}">
+              <h6 class="track__name">${parsedArray[i].trackName}</h6>
+              <h5 class="artist__name">${parsedArray[i].artistName}</h5>
+            </div>
+          `)
+        }
+      })
+  }
+
+  $(document).on('click', '.track', function (event) {
+    console.log(event.currentTarget.dataset.preview)
+    $('.audio').html('')
+    $('.audio').html(`
+      <source src="${event.currentTarget.dataset.preview}">
+    `).trigger('load').trigger('play')
+
+    // $('.audio').load()
+  })
+
+  $(document).on('submit', '#search-music', function (event) {
+    event.preventDefault()
+    getAndSetValue()
+  })
+})
